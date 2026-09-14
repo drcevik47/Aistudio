332578ac8bdfe1852fcd24f7994cbdecfc729fb4
drcevik47 <drcevik47@gmail.com>
2026-09-14 15:46:48 +0300

    refactor: remove maintenance and auxiliary scripts
    Delete unused utility scripts, test logs, and build helper tools to clean up the repository structure.

--- test_crash.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-logcat -d | grep -i -E "fatal|exception|error" | tail -n 100
--- test_okx.py
+++
@@ -1,7 +0,0 @@
-import hmac
-import base64
-import time
-import datetime
-from urllib.parse import urlparse
-
-# Just a script to double check signature generation rules.
